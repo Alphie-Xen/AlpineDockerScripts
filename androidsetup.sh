4e8006f44d0d99beb8ed614ccb29edee24e54ebf
Alphie-Xen <Davisonal03@gmail.com>
2025-12-17 11:16:38 +0000

    Update androidsetup.sh

--- androidsetup.sh
+++ androidsetup.sh
@@ -5,11 +5,8 @@ apk add qemu-img
 mkdir qemu-android
 cd qemu-android
 
-wget -O android.iso https://archive.org/download/sjarb_android_5.1
-RC1/android-x86-5.1-rc1.iso
+wget -O android.iso https://archive.org/download/sjarb_android_5.1RC1/android-x86-5.1-rc1.iso
 
 qemu-img create -f qcow2 disk.qcow2 20G
 
-qemu-system-x86_64 -smp 4 -m 4G  -drive file=disk.qcow2,format=qco
-w2,if=ide,index=0 -cdrom android.iso  &
-
+qemu-system-x86_64 -smp 4 -m 4G  -drive file=disk.qcow2,format=qcow2,if=ide,index=0 -cdrom android.iso  &
